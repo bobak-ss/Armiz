f22c11e611ef32de1e752d2c1457b484435ee072
babak_ss <ssalimib@gmail.com>
2020-06-01 12:03:07 +0430

    Allies shoot bullet to enemy on timer and on player tap

--- Assets/Scripts/GameView.cs
+++ Assets/Scripts/GameView.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class GameView: MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class GameView: MonoBehaviour
 
     [Header("Game Prameters")]
     public float attackTime;
+    public float shootTime;
     public Vector3 enemyPos;
 
     [Header("Editor Prameters")]
@@ -17,8 +19,11 @@ public class GameView: MonoBehaviour
     [Header("Prefabs")]
     public GameObject allyPrefab;
     public GameObject enemyPrefab;
+    public GameObject bulletPrefab;
 
     private Timer attackTimer;
+    private Timer shootTimer;
+    private List<Vector3> alliesPosList;
     
     void Awake()
     {
@@ -47,7 +52,16 @@ public class GameView: MonoBehaviour
                 }
                 else
                 {
-                    // attacking stuff!
+                    if (shootTimer.isFinished(Time.time))
+                    {
+                        shootTimer = new Timer(Time.time, shootTime);
+                        ShootBulletsAllies();
+                    }
+                    
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        ShootBulletsAllies();
+                    }
                 }
                 break;
             case GameModel.GameState.Idle:
@@ -61,8 +75,12 @@ public class GameView: MonoBehaviour
     public void OnAttackBtnClick()
     {
         uiPanel.SetActive(false);
+
         attackTimer = new Timer(Time.time, attackTime);
         gameController.StartAttack();
+
+        shootTimer = new Timer(Time.time, shootTime);
+        ShootBulletsAllies();
     }
 
     public void OnAttackTimerFinished()
@@ -73,14 +91,15 @@ public class GameView: MonoBehaviour
 
     public void SpawnAllies()
     {
+        alliesPosList = new List<Vector3>();
         float segmentDegree = 360 / gameModel.allyCount;
         int radius = (gameModel.allyCount < 20) ? 2 : 5;
         for (int i = 0; i < gameModel.allyCount; i++)
         {
-            ObjectPool.Spawn(allyPrefab, new Vector3(enemyPos.x + Utility.rCos(radius, i * segmentDegree),
-                                                    enemyPos.y * allyPrefab.transform.localScale.y,
-                                                    enemyPos.z + Utility.rSin(radius, i * segmentDegree))
-                            );
+            alliesPosList.Add(new Vector3(enemyPos.x + Utility.rCos(radius, i * segmentDegree),
+                                            enemyPos.y * allyPrefab.transform.localScale.y,
+                                            enemyPos.z + Utility.rSin(radius, i * segmentDegree)));
+            ObjectPool.Spawn(allyPrefab, alliesPosList[i]);
         }
     }
 
@@ -90,8 +109,13 @@ public class GameView: MonoBehaviour
         ObjectPool.Spawn(enemyPrefab, enemyPos);
     }
 
-    public void Log(string s)
+    public void ShootBulletsAllies()
     {
-        Debug.Log(s);
+        for (int i = 0; i < alliesPosList.Count; i++)
+        {
+            GameObject bulletGO = ObjectPool.Spawn(bulletPrefab, alliesPosList[i]);
+            Tween thisTween = bulletGO.transform.DOMove(enemyPos, 0.7f);
+            thisTween.OnComplete(() => ObjectPool.Despawn(bulletGO));
+        }
     }
 }
