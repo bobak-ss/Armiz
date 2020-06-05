414d8187b695f9c966a38747df82d0b9442e3736
babak_ss <ssalimib@gmail.com>
2020-06-06 00:02:34 +0430

    Fighter system

---
+++ Assets/_Armiz/Scripts/Fighter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Fighter : ScriptableObject
+{
+    [SerializeField] private int _damage;
+    [SerializeField] private int _currentHealth;
+    [SerializeField] private int _totalHealth;
+    [SerializeField] private FighterType _fighterType;
+
+    public bool Damage(int amount)
+    {
+        _currentHealth = Math.Max(0, _currentHealth - amount);
+        return _currentHealth == 0;
+    }
+
+    public void ResetHealth()
+    {
+        _currentHealth = _totalHealth;
+    }
+
+    public int GetDamage() { return _damage; }
+
+    public int GetCurrentHealth() { return _currentHealth; }
+    public enum FighterType
+    {
+        Enemy,
+        Ally
+    }
+}
